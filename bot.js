a312b785ae48cb226ab9ee55327107011620e9cb
Jakob Lentz <jakob.lentz@notch-interactive.com>
2016-09-23 11:10:48 +0200

    create help command

--- bot.js
+++ bot.js
@@ -205,8 +205,17 @@ bot.on('message', message => {
 	if (message.content === '-rpg') {
 		let m = "```xl\n"
 		m+= `"|---------RPG HELP---------|"\n`
+		m+= ` > welcome to the Cyberpunk RPG\n`
+		m+= ` > this is the help page\n`
 		m+= ` > \n`
+		m+= ` > Commands:\n`
+		m+= ` > -create // "start your adventure!"\n`
+		m+= ` > -profile, -prf // "checks your profile"\n`
+		m+= ` > -inventory, -inv // "shows your inventory"\n`
+		m+= ` > -daily, -dly // "collect your daily rewards"\n`
 		m+= ` > \n`
+		m+= ` > Infos:\n`
+		m+= ` > Daily reset is everyday at 12:00 UTC+01:00\n`
 		m+= `"|--------------------------|"`
 		m+= "```"
 	}
