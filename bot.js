c96e3b0536c9164429c84db39b49b43076dfed30
Jakob Lentz <jakob.lentz@notch-interactive.com>
2016-09-23 11:02:41 +0200

    db update kill old db Kill all semicolons Dokumentation update

--- bot.js
+++ bot.js
@@ -1,50 +1,50 @@
-console.log ('<== STARTING BOT ==>');
+console.log ('<== STARTING BOT ==>')
 
-const Discord = require('discord.js'); //discord
-const request = require('request'); //jason request from website
-const Twitter = require('twitter'); //twitter api
-const fs = require('fs'); //filesystem
-const jsonfile = require('jsonfile'); //json write/reload
-const rand = require('random-int'); //number randomizer finally intuitive
-const cronJob = require('cron').CronJob; //scheduler
+const Discord = require('discord.js') //discord
+const request = require('request') //jason request from website
+const Twitter = require('twitter') //twitter api
+const fs = require('fs') //filesystem
+const jsonfile = require('jsonfile') //json write/reload
+const rand = require('random-int') //number randomizer finally intuitive
+const cronJob = require('cron').CronJob //scheduler
 
-console.log ('node modules √');
+console.log ('node modules √')
 
-const bot = new Discord.Client();
-const dbpath = './db.json';
+const bot = new Discord.Client()
+const dbpath = './db.json'
 
 
-var db = require("./db.json");
-var cred = require("./cred.json");
+var db = require("./db.json")
+var cred = require("./cred.json")
 var client = new Twitter({
 	consumer_key: cred.consumer,
 	consumer_secret: cred.consumersecret,
 	access_token_key: cred.twittertoken,
 	access_token_secret: cred.twittertokensecret
-});
+})
 
-console.log ('variables √');
+console.log ('variables √')
 
 
 bot.on('ready', () => {
-	console.log('<== BOT STARTED ==>');
-});
+	console.log('<== BOT STARTED ==>')
+})
 
 //DEBUG/ADMIN ---------------------------------------------
 
 bot.on('message', message => {
-	if (message.author.id === bot.user.id) return;
+	if (message.author.id === bot.user.id) return
 	if (message.content.startsWith('-name'))
 	{
-		let m = `Hello ${message.content.slice(6)}`;
-		message.channel.sendMessage(m);
+		let m = `Hello ${message.content.slice(6)}`
+		message.channel.sendMessage(m)
 	}
 	if (message.content.startsWith('.eval') && message.author.id === '64438454750031872' || message.content.startsWith('.eval') && message.author.id === '148764744231157760') {
 		try {
-			const com = eval(message.content.split(" ").slice(1).join(" "));
-			message.channel.sendMessage('```\n' + com + '```');
+			const com = eval(message.content.split(" ").slice(1).join(" "))
+			message.channel.sendMessage('```\n' + com + '```')
 		} catch(e) {
-			message.channel.sendMessage('```\n' + e + '```');
+			message.channel.sendMessage('```\n' + e + '```')
 		}
 	}
 
@@ -52,26 +52,26 @@ bot.on('message', message => {
 	if (message.content.startsWith('-comp'))
 	{
 		let battletag = message.content.slice(7)
-		let url = 'https://api.lootbox.eu/pc/eu/' + battletag.replace('#','-') + '/competitive-play/allHeroes/';
-		message.channel.sendMessage(url);
-		message.channel.sendMessage('loading from DB...');
+		let url = 'https://api.lootbox.eu/pc/eu/' + battletag.replace('#','-') + '/competitive-play/allHeroes/'
+		message.channel.sendMessage(url)
+		message.channel.sendMessage('loading from DB...')
 		request({
 			url: url,
 			json: true
 		}, function (error, response, body){
 			if (!error && response.statusCode === 200){
-				body = JSON.stringify(body);
-				body = body.replace(/-/g, "");
-				body = JSON.parse(body);
-				let kd = body.Eliminations;
-				let Winrate = body.GamesWon / body.GamesPlayed * 100;
-				let m = '';
-				m += '```xl\n';
-				m += `${battletag} "Competitive Profile"\n`;
+				body = JSON.stringify(body)
+				body = body.replace(/-/g, "")
+				body = JSON.parse(body)
+				let kd = body.Eliminations
+				let Winrate = body.GamesWon / body.GamesPlayed * 100
+				let m = ''
+				m += '```xl\n'
+				m += `${battletag} "Competitive Profile"\n`
 				m += `Comp Games Winrate: ${Winrate}%\n`
 				m += `Comp Games Played: ${body.GamesPlayed}\n`
 				m += `Comp Games Won: ${body.GamesWon}\n`
-				m += `Eliminations: ${body.Eliminations}\n`;
+				m += `Eliminations: ${body.Eliminations}\n`
 				m += `Kills/Death: ${body.EliminationsAverage}\n`
 				m += `Damage: ${body.DamageDone}\n`
 				m += `Heal: ${body.HealingDone}\n`
@@ -80,7 +80,7 @@ bot.on('message', message => {
 				m += `-Silver Medals: ${body.MedalsSilver}\n`
 				m += `-Bronze Medals: ${body.MedalsBronze}\n`
 				m += '```'
-				message.channel.sendMessage(m);
+				message.channel.sendMessage(m)
 			}
 		})
 	}
@@ -88,27 +88,27 @@ bot.on('message', message => {
 	if (message.content.startsWith('-quick'))
 	{
 		let battletag = message.content.slice(7)
-		let url = 'https://api.lootbox.eu/pc/eu/' + battletag.replace('#','-') + '/quick-play/allHeroes/';
-		message.channel.sendMessage(url);
-		message.channel.sendMessage('loading from DB...');
+		let url = 'https://api.lootbox.eu/pc/eu/' + battletag.replace('#','-') + '/quick-play/allHeroes/'
+		message.channel.sendMessage(url)
+		message.channel.sendMessage('loading from DB...')
 		request({
 			url: url,
 			json: true
 		}, function (error, response, body){
 			if (!error && response.statusCode === 200){
-				body = JSON.stringify(body);
-				body = body.replace(/-/g, "");
-				body = JSON.parse(body);
-				let kd = body.Eliminations;
-				let Winrate = body.GamesWon / body.GamesPlayed * 100;
-				let m = '';
-				m += '```xl\n';
-				m += `${battletag} "Quick Profile"\n`;
+				body = JSON.stringify(body)
+				body = body.replace(/-/g, "")
+				body = JSON.parse(body)
+				let kd = body.Eliminations
+				let Winrate = body.GamesWon / body.GamesPlayed * 100
+				let m = ''
+				m += '```xl\n'
+				m += `${battletag} "Quick Profile"\n`
 				m += `Quick Games Winrate: ${Winrate}%\n`
 				m += `Quick Games Played: ${body.GamesPlayed}\n`
 				m += `Quick Games Won: ${body.GamesWon}\n`
 				m += `Time Played Quick: ${body.TimePlayed}\n`
-				m += `Eliminations: ${body.Eliminations}\n`;
+				m += `Eliminations: ${body.Eliminations}\n`
 				m += `Kills/Death: ${body.EliminationsAverage}\n`
 				m += `Damage: ${body.DamageDone}\n`
 				m += `Heal: ${body.HealingDone}\n`
@@ -117,7 +117,7 @@ bot.on('message', message => {
 				m += `-Silver Medals: ${body.MedalsSilver}\n`
 				m += `-Bronze Medals: ${body.MedalsBronze}\n`
 				m += '```'
-				message.channel.sendMessage(m);
+				message.channel.sendMessage(m)
 			}
 		})
 	}
@@ -125,18 +125,18 @@ bot.on('message', message => {
 	if (message.content.startsWith('-rawquick'))
 	{
 		let battletag = message.content.slice(10)
-		let url = 'https://api.lootbox.eu/pc/eu/' + battletag.replace('#','-') + '/quick-play/allHeroes/';
-		message.channel.sendMessage(url);
-		message.channel.sendMessage('loading from DB...');
+		let url = 'https://api.lootbox.eu/pc/eu/' + battletag.replace('#','-') + '/quick-play/allHeroes/'
+		message.channel.sendMessage(url)
+		message.channel.sendMessage('loading from DB...')
 		request({
 			url: url,
 			json: true
 		}, function (error, response, body){
 			if (!error && response.statusCode === 200){
-				body = JSON.stringify(body);
-				body = body.replace(/-/g, "");
-				body = body.replace(/","/g, '",\n"');
-				message.channel.sendMessage("```json\n" + body + "```");
+				body = JSON.stringify(body)
+				body = body.replace(/-/g, "")
+				body = body.replace(/","/g, '",\n"')
+				message.channel.sendMessage("```json\n" + body + "```")
 			}
 		})
 	}
@@ -144,38 +144,39 @@ bot.on('message', message => {
 	if (message.content.startsWith('-rawcomp'))
 	{
 		let battletag = message.content.slice(10)
-		let url = 'https://api.lootbox.eu/pc/eu/' + battletag.replace('#','-') + '/competitive-play/allHeroes/';
-		message.channel.sendMessage(url);
-		message.channel.sendMessage('loading from DB...');
+		let url = 'https://api.lootbox.eu/pc/eu/' + battletag.replace('#','-') + '/competitive-play/allHeroes/'
+		message.channel.sendMessage(url)
+		message.channel.sendMessage('loading from DB...')
 		request({
 			url: url,
 			json: true
 		}, function (error, response, body){
 			if (!error && response.statusCode === 200){
-				body = JSON.stringify(body);
-				body = body.replace(/-/g, "");
-				body = body.replace(/","/g, '",\n"');
-				message.channel.sendMessage("```json\n" + body + "```");
+				body = JSON.stringify(body)
+				body = body.replace(/-/g, "")
+				body = body.replace(/","/g, '",\n"')
+				message.channel.sendMessage("```json\n" + body + "```")
 			}
 		})
 	}
 
 	//TWITTER ----------------------------------------------
 	if (message.content.startsWith('-tweet') && message.author.id === '64438454750031872' || message.content.startsWith('-tweet') && message.author.id === '148764744231157760'){
-		let tweetbody = message.content.slice(7);
+		let tweetbody = message.content.slice(7)
 		if (tweetbody.length <= 140) {
 			client.post('statuses/update', {status: tweetbody},  function(error, tweet, response) {
-				message.channel.sendMessage('Sucessfully tweeted:"' + tweetbody + '" to: https://twitter.com/OverSwiss');
+				message.channel.sendMessage('Sucessfully tweeted:"' + tweetbody + '" to: https://twitter.com/OverSwiss')
 			})
 		} else {
-			message.channel.sendMessage("Your tweet is longer than 140 letters. Pls shorten your tweet.");
+			message.channel.sendMessage("Your tweet is longer than 140 letters. Pls shorten your tweet.")
 		}
 	}
-});
+})
 
 // RPG ---------------------------------------------------------
 /*
-MATH DOKU-------------------
+<============================= DOKUMENTATION
+
 <==EXP CURVE==>
 x = 10 * (1.5 ^ y)
 y = log(x/10) / log(1.5)
@@ -183,29 +184,45 @@ y = log(x/10) / log(1.5)
 x = exp
 y = lvl
 --
-.eval Math.log(EXP / 10) / Math.log(1.5);
-.eval 10 * Math.pow(1.5, LVL);
-<== ///// ==>
+.eval Math.log(EXP / 10) / Math.log(1.5)
+.eval 10 * Math.pow(1.5, LVL)
+
+<==Message Template==>
+let m = "```xl\n"
+m+= `"|---------"---------|"\n`
+m+= ` > \n`
+m+= ` > \n`
+m+= `"|-------------------|"`
+m+= "```"
+
+
 */
 
 
 bot.on('message', message => {
-	if (message.author.id === bot.user.id) return;
+	if (message.author.id === bot.user.id) return
 
-
+	if (message.content === '-rpg') {
+		let m = "```xl\n"
+		m+= `"|---------RPG HELP---------|"\n`
+		m+= ` > \n`
+		m+= ` > \n`
+		m+= `"|--------------------------|"`
+		m+= "```"
+	}
 	///////////////////// FOR TESTING
 	if (message.content === '-reset' && message.author.id === '64438454750031872' || message.content.startsWith('-reset') && message.author.id === '148764744231157760'){
 		for(i in db){
-			db[i].daily = 0;
+			db[i].daily = 0
 		}
-		jsonfile.writeFile(dbpath, db);
-		message.channel.sendMessage("daily reset!");
+		jsonfile.writeFile(dbpath, db)
+		message.channel.sendMessage("daily reset!")
 	}
 
 	if (message.content === '-freeexp' && message.author.id === '64438454750031872' || message.content.startsWith('-freeexp') && message.author.id === '148764744231157760'){
-		let dailyexp = rand(100, 1000);
-		db[message.author.id].exp += dailyexp;
-		message.channel.sendMessage(`added ${dailyexp} exp`);
+		let dailyexp = rand(100, 1000)
+		db[message.author.id].exp += dailyexp
+		message.channel.sendMessage(`added ${dailyexp} exp`)
 	}
 	////////////////////////////////////
 
@@ -214,82 +231,82 @@ bot.on('message', message => {
 		cronTime: '00 00 12 * * *',
 		onTick: function reset(){
 			for(i in db){
-				db[i].daily = 0;
+				db[i].daily = 0
 			}
 			console.log("reseted the db")
-			jsonfile.writeFile(dbpath, db);
+			jsonfile.writeFile(dbpath, db)
 		},
 		start: false,
 		timeZone: "Europe/Berlin"
-	});
-	dailyreset.start();
+	})
+	dailyreset.start()
 
 	// daily
-	if (message.content === '-daily'){
+	if (message.content === '-daily' || message.content === '-dly'){
 		if (db[message.author.id]) {
 			if (db[message.author.id].daily === 0) {
-				let dailyexp = rand(100, 1000);
-				let dailycredits = rand(100, 1000);
-				db[message.author.id].exp += dailyexp;
-				db[message.author.id].credits += dailycredits;
-				db[message.author.id].daily = 1;
-				jsonfile.writeFile(dbpath, db);
-				let m = "```xl\n";
-				m+= `"|---------Daily reward!---------|"\n`;
-				m+= `||Credits: +${dailycredits}\n`;
-				m+= `||Exp: +${dailyexp}\n`;
-				m+= `"|-------------------------------|"`;
-				m+= "```";
-				message.channel.sendMessage(m);
+				let dailyexp = rand(100, 1000)
+				let dailycredits = rand(100, 1000)
+				db[message.author.id].exp += dailyexp
+				db[message.author.id].credits += dailycredits
+				db[message.author.id].daily = 1
+				jsonfile.writeFile(dbpath, db)
+				let m = "```xl\n"
+				m+= `"|---------Daily reward!---------|"\n`
+				m+= ` > Credits: +${dailycredits}\n`
+				m+= ` > Exp: +${dailyexp}\n`
+				m+= `"|-------------------------------|"`
+				m+= "```"
+				message.channel.sendMessage(m)
 			} else {
-				message.channel.sendMessage("You already collected your dailies!");
+				message.channel.sendMessage("You already collected your dailies!")
 			}
 		} else {
-			message.channel.sendMessage("No profile found. Use ``-create`` to create one.");
+			message.channel.sendMessage("No profile found. Use ``-create`` to create one.")
 		}
 	}
 	// SHOW INVENTORY
-	if (message.content === '-inventory'){
+	if (message.content === '-inventory' || message.content === '-inv' ){
 		if (db[message.author.id]) {
 			let m = '```xl\n'
 			m += `"|---------INVENTORY---------|"\n`
 			for(i in db[message.author.id].inventory){
 				m += ` > `
-				m += db[message.author.id].inventory[i];
+				m += db[message.author.id].inventory[i]
 				m += `\n`
 			}
 			m += '"|---------------------------|"```'
-			message.channel.sendMessage(m);
+			message.channel.sendMessage(m)
 		}
 	}
 
 	// SHOW CURRENT PROFILE
-	if (message.content === '-profile'){
+	if (message.content === '-profile' || message.content === '-prf'){
 		if (db[message.author.id]) {
-			let credits = db[message.author.id].credits;
-			let exp = db[message.author.id].exp;
-			let m = "```xl\n";
-			m+= `"|---------PROFILE---------|"\n`;
-			m+= `||Credits: ${credits}\n`;
-			m+= `||Exp: ${exp}\n`;
-			m+= `"|-------------------------|"`;
-			m+= "```";
-			message.channel.sendMessage(m);
+			let credits = db[message.author.id].credits
+			let exp = db[message.author.id].exp
+			let m = "```xl\n"
+			m+= `"|---------PROFILE---------|"\n`
+			m+= ` > Credits: ${credits}\n`
+			m+= ` > Exp: ${exp}\n`
+			m+= `"|-------------------------|"`
+			m+= "```"
+			message.channel.sendMessage(m)
 		} else {
-			message.channel.sendMessage("No profile found. Use ``-create`` to create one.");
+			message.channel.sendMessage("No profile found. Use ``-create`` to create one.")
 		}
 	}
 	// CREATE NEW PROFILE
 	if (message.content === '-create'){
 		if (db[message.author.id]) {
-			message.channel.sendMessage("You already have a profile");
+			message.channel.sendMessage("You already have a profile")
 		} else {
-			db[message.author.id] = {"credits":0,"exp":0,"daily":0};
-			jsonfile.writeFile(dbpath, db);
-			message.channel.sendMessage("New profile created!");
-			console.log ('new profile created!');
+			db[message.author.id] = {"credits":0,"exp":0,"daily":0}
+			jsonfile.writeFile(dbpath, db)
+			message.channel.sendMessage("New profile created!")
+			console.log ('new profile created!')
 		}
 	}
-});
-bot.login(cred.bottoken);
-console.log ('login √');
+})
+bot.login(cred.bottoken)
+console.log ('login √')
