129cf1b688ac1d50127a2874442634cd07e51b58
Jakob Lentz <jakob.lentz@notch-interactive.com>
2016-09-28 17:11:28 +0200

    giveitem update

--- bot.js
+++ bot.js
@@ -321,10 +321,12 @@ bot.on('message', message => {
 	}
 
 	// FREE GOLD BAR (TESTING ONLY)
-	if (message.content === '-freegoldbar' && message.author.id === '64438454750031872' || message.content.startsWith('.eval') && message.author.id === '148764744231157760'){
+
+	if (message.content.startsWith('-giveitem') && message.author.id === '64438454750031872' || message.content.startsWith('-giveitem') && message.author.id === '148764744231157760'){
 		for(i in db[message.author.id].inventory){
 			if (db[message.author.id].inventory[i] === "-"){
- 				db[message.author.id].inventory[i] = "GoldBar"
+ 				db[message.author.id].inventory[i] = message.content.slice(10)
+				message.channel.sendMessage(`gave 1 of ${message.content.slice(10)}`)
 				return
 			}
 		}
@@ -360,7 +362,7 @@ bot.on('message', message => {
 		let m = '```markdown\n'
 		m += `#==========INVENTORY==========#\n`
 		for(i in db[message.author.id].inventory){
-			m += `+ `
+			m += `${i}. `
 			m += db[message.author.id].inventory[i]
 			m += `\n`
 		}
