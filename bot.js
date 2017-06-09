2657bf4d40858bd4a5b20a1aef81b5d4eb8be49e
Jakob Lentz <jakob.lentz@notch-interactive.com>
2017-06-09 17:26:07 +0200

    new stuff

--- bot.js
+++ bot.js
@@ -22,8 +22,6 @@ console.log('<== STARTING BOT ==>');
 // Initialisation
 jsonfile.spaces = 2
 
-console.log('Setup √')
-
 // listeners
 bot.on('message', (message) => {
 
@@ -34,30 +32,7 @@ bot.on('message', (message) => {
 	if (message.author.bot) return
 
 	// Bot mentioned
-	if (message.isMentioned(bot.user.id)) {
-		const embed = new discord.RichEmbed()
-		.setDescription('Bot got mentioned')
-		.setTimestamp()
-		.addField('User' ,`${message.author.username} (${message.author.id})`)
-		.addField('Server' ,`${message.guild.name}`)
-		.addField('Message' ,`${message.cleanContent}`)
-		.setColor('#0000ff')
-		discordLog(embed)
-    }
-
-	// Console
-	if (message.channel.id === "308901535989891072" && message.author.id === '64438454750031872') {
-		try {
-			const com = eval(message.content)
-			const embed = new discord.RichEmbed()
-			.setTitle(com)
-			message.channel.sendEmbed(embed)
-		} catch (err) {
-			const embed = new discord.RichEmbed()
-			.setTitle(err)
-			message.channel.sendEmbed(embed)
-		}
-	}
+	if (message.isMentioned(bot.user.id)) {}
 
 	// Execute command
 	if (!message.content.startsWith(settings.prefix)) return
@@ -67,14 +42,6 @@ bot.on('message', (message) => {
 		let cmdFile = require('./commands/' + command)
 		cmdFile.run(bot, message, args, discord, settings)
 	} catch (err) {
-		const embed = new discord.RichEmbed()
-		.setDescription('Error!!')
-		.setTimestamp()
-		.addField('User' ,`${message.author.username} (${message.author.id})`)
-		.addField('Server' ,`${message.guild.name}`)
-		.addField('Error' ,err)
-		.setColor('#ff0000')
-		discordLog(embed)
 		console.log(err)
 	}
 })
@@ -85,19 +52,6 @@ bot.on('guildMemberAdd', member => {
 	}
 })
 
-console.log('Commands loaded √')
-
-// Ready
-bot.on('ready', () => {
-	const embed = new discord.RichEmbed()
-	.setTimestamp()
-	.addField('Users' , bot.users.size)
-	.addField('Servers' , bot.guilds.size)
-	.addField('Channels' , bot.channels.size)
-	.setColor('#00ff00')
-	discordLog(embed)
-	console.log('<== BOT ONLINE ==>')
-});
-
 // Login
 bot.login(settings.bottoken)
+console.log('<== BOT ONLINE ==>');
--- selfbot.js
+++ selfbot.js
@@ -14,18 +14,18 @@ bot.on('message', message => {
     if (message.cleanContent.startsWith("💻"))
         try {
             const com = eval(message.content.slice(2))
-            message.channel.sendMessage(":white_check_mark: " + com)
+            message.channel.sendMessage("✅ " + com)
         } catch (err) {
-            message.channel.sendMessage(":x:" + err)
+            message.channel.sendMessage("❌" + err)
         }
 
     if (message.content.startsWith("🍪")) {
-        message.channel.sendMessage("Zer0 gave a cookie to **(っ◔◡◔)っ :cookie:** " + message.content.slice(2))
+        message.channel.sendMessage("✅ Zer0 gave a cookie to **(っ◔◡◔)っ :cookie:** " + message.content.slice(2))
     }
 
     if (message.content.startsWith("🌐")) {
         urban(message.content.slice(2)).then((result) => {
-            message.channel.sendMessage(":globe_with_meridians: **Definition: **" + result.definition)
+            message.channel.sendMessage("✅ **Definition: **" + result.definition)
         })
     }
 
@@ -54,66 +54,105 @@ bot.on('message', message => {
             "Very doubtful"
         ]
         var item = answers[Math.floor(Math.random() * answers.length)];
-        message.channel.sendMessage(":8ball: ** ( ‾ʖ̫‾)つ──☆*:・ﾟ **" + item)
+        message.channel.sendMessage("✅ ** ( ‾ʖ̫‾)つ──☆*:・ﾟ **" + item)
     }
 
     if (message.content.startsWith("🎲")) {
-        let rand = Math.random() * 100
-        let choice = message.content.slice(3)
 
         if (message.content.startsWith("🎲 odds")) {
-            message.channel.sendMessage("Your odds are: 40% ⚫/🔴 and 20% 🔵")
+            message.channel.sendMessage("⚫⚫⚫⚫-🔴🔴🔴🔴-🔵🔵")
             return
         }
 
-        if (choice === "black") {
+		if (message.content.startsWith("🎲 global")) {
+			let rand = Math.trunc(Math.random() * 100)
+			if (rand < 69 ) {
+				message.channel.sendMessage("✝ Lost! You rolled a "+ rand +". **" + db.globalpot + "** coins in Globalpot. ✝")
+				return
+			} else if (rand > 80) {
+				message.channel.sendMessage("✝ Lost! You rolled a "+ rand +". **" + db.globalpot + "** coins in Globalpot. ✝")
+				return
+			} else {
+				message.channel.sendMessage("🔥🔥🔥 Just snagged the global pot with a " + rand + " ROLL. Won " + db.globalpot + "coins 🔥🔥🔥")
+				return
+			}
+			return
+		}
+
+		let rand = Math.random() * 100
+        let args = message.content.split(" ")
+		let pot = parseInt(args[2])
+
+		if (args.length === 1) {
+			message.channel.sendMessage("❌ **Usage:*** 🎲 <black/red/blue> <amount>")
+			return
+		} else if (args.length === 2) {
+			message.channel.sendMessage("❌ **Usage:*** 🎲 <black/red/blue> <amount>")
+			return
+		}
+
+		if (pot > db.coins) {
+			message.channel.sendMessage("❌ Not enough 💰")
+			return
+		}
+
+
+        if (args[1] === "black") {
             test()
             return
-        } else if (choice === "red") {
+        } else if (args[1] === "red") {
             test()
             return
-        } else if (choice === "blue") {
+        } else if (args[1] === "blue") {
             test()
             return
         } else {
-            message.channel.sendMessage("please specify your predicition (black/red/blue)")
+            message.channel.sendMessage("❌ **Usage:*** 🎲 <black/red/blue> <amount>")
             return
         }
 
         function test() {
             if (rand <= 40) {
-                if (choice === "black") {
-                    db.coins = db.coins * 2
+                if (args[1] === "black") {
+                    db.coins = db.coins + pot
                     message.channel.sendMessage("⚫ you won! current balance **" + db.coins + "**")
 
                 } else {
-                    db.coins = db.coins / 2
+                    db.coins = db.coins - pot
+					db.globalpot = db.globalpot + pot
                     message.channel.sendMessage("⚫ you lost! current balance **" + db.coins + "**")
 
                 }
             } else if (rand <= 80) {
-                if (choice === "red") {
-                    db.coins = db.coins * 2
+                if (args[1] === "red") {
+                    db.coins = db.coins + pot
                     message.channel.sendMessage("🔴 you won! current balance **" + db.coins + "**")
 
                 } else {
-                    db.coins = db.coins / 2
+                    db.coins = db.coins - pot
+					db.globalpot = db.globalpot + pot
                     message.channel.sendMessage("🔴 you lost! current balance **" + db.coins + "**")
 
                 }
             } else if (rand <= 100) {
-                if (choice === "blue") {
-                    db.coins = db.coins * 4
+                if (args[1] === "blue") {
+                    db.coins = db.coins + pot * 2
                     message.channel.sendMessage("🔵 you won! current balance **" + db.coins + "**")
 
                 } else {
-                    db.coins = db.coins / 4
+                    db.coins = db.coins - pot
+					db.globalpot = db.globalpot + pot
                     message.channel.sendMessage("🔵 you lost! current balance **" + db.coins + "**")
                 }
             }
-			fs.writeFile(dbPath, JSON.stringify(db))
+            fs.writeFile(dbPath, JSON.stringify(db))
         }
     }
+
+	if (message.content.startsWith("💰")) {
+		message.channel.sendMessage("✅ You have " + db.coins)
+	}
+
 });
 
 console.log('<== ONLINE ==>');
