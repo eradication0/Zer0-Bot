afd7c96fe66a111e8aa78581420bf87bfcbbcb40
Jakob Lentz <jakob.lentz@notch-interactive.com>
2017-11-24 15:32:18 +0100

    lootdrop update, drops.json, rules

--- commands/lllll.js
+++ commands/lllll.js
@@ -17,15 +17,24 @@ exports.run = function(bot, message, args, discord, settings, inv) {
 		let prizes = 0
 		if (randPrizes <= 30) { prizes = 1 }
 		else if (randPrizes <= 50) { prizes = 2 }
-		else if (randPrizes <= 70) { prizes = 3 }
-		else if (randPrizes <= 90) { prizes = 4 }
+		else if (randPrizes <= 80) { prizes = 3 }
+		else if (randPrizes <= 99) { prizes = 4 }
 		else if (randPrizes <= 100) { prizes = 5 }
 		console.log(true)
-		let embed = new discord.RichEmbed().setTitle("Your lootbox contains **" + prizes + "** drops").setColor("#006FEC")
+		let embed = new discord.RichEmbed().setTitle("(command testing) Your lootbox contains **" + prizes + "** drops").setColor("#006FEC")
 		message.channel.send({embed})
+		const drops = require('./drops.json')
 		for (var i = 0; i < prizes; i++) {
+			let randDrop = rand(0,100)
+			let rarity = 0
+			if (randDrop <= 60) {rarity = 1}
+			else if (randDrop <= 70) { rarity = 2 }
+			else if (randDrop <= 90) { rarity = 3 }
+			else if (randDrop <= 99) { rarity = 4 }
+			else if (randDrop <= 100) { rarity = 5 }
 			setTimeout(function () {
-				let embed = new discord.RichEmbed().setTitle("🎇").setColor("#006FEC")
+				let actualDrop = rand(0,drops[rarity].length)
+				let embed = new discord.RichEmbed().setTitle("actualDrop").setColor(drops[rarity].color)
 				message.channel.send({embed})
 			}, i * 200);
 		}
