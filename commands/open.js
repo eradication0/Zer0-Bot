5457846d0f4747cc477f7d853cfb48559400f07e
Zer0 <jakob.minka@gmail.com>
2017-11-24 12:39:39 +0100

    more debug

--- commands/open.js
+++ commands/open.js
@@ -1,18 +1,15 @@
 exports.run = function(bot, message, args, discord, settings, inv) {
-	console.log("1")
 	createUser(message)
-	console.log("2")
 	// get user id
 	let userid = message.author.id
 	if (userid.startsWith("!")) {
 		userid = userid.slice(1)
 	}
-	console.log("3")
+	console.log(inv.userid.boxes)
 
 	if (inv.userid.boxes === 0) {
 		let embed = new discord.RichEmbed().setTitle("You dont have a lootbox :^) get gud").setColor("#E54C4C")
 		message.channel.send({embed})
-		console.log("4")
 	} else {
 		let randPrizes = rand(0,100)
 		let prizes = 0
@@ -21,7 +18,6 @@ exports.run = function(bot, message, args, discord, settings, inv) {
 		else if (randPrizes <= 70) { prizes = 3 }
 		else if (randPrizes <= 90) { prizes = 4 }
 		else if (randPrizes <= 100) { prizes = 5 }
-		console.log("5")
 		let embed = new discord.RichEmbed().setTitle("Your lootbox contains **" + randPrizes + "** drops").setColor("#006FEC")
 		message.channel.send({embed})
 		for (var i = 0; i < randPrizes; i++) {
