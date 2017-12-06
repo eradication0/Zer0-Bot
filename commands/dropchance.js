c4fa97f1d7329a0e296bfada2b9f90cbb6a3a6db
Jakob Lentz <jakob.lentz@notch-interactive.com>
2017-12-06 15:07:33 +0100

    fixed layout

--- commands/dropchance.js
+++ commands/dropchance.js
@@ -2,10 +2,10 @@ exports.run = function(bot, message, args, discord) {
 		const embed = new discord.RichEmbed().setColor('#6DC066')
 		.setTitle('Lootlist')
 		.addField('Box Droprate','📦 0.6% per message')
-		.addField('Common','💩 40%',true)
-		.addField('Uncommon','💚 30%',true)
-		.addField('Rare','💙 20%',true)
-		.addField('Epic','💜  9%',true)
-		.addField('Legendary','💛  1%',true)
+		.addField('Common','💩 40%')
+		.addField('Uncommon','💚 30%')
+		.addField('Rare','💙 20%')
+		.addField('Epic','💜  9%')
+		.addField('Legendary','💛  1%')
 		message.channel.send({ embed });
 }
