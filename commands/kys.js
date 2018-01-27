3084cf300937cb1073fdeda20e79119d013e7bae
Zer0 <jakob.minka@gmail.com>
2018-01-27 16:10:45 +0100

    added status command

--- commands/kys.js
+++
@@ -1,5 +0,0 @@
-exports.run = function(bot, message, args, discord) {
-		const embed = new discord.RichEmbed().setColor('#6DC066')
-		.setTitle('no u!')
-		message.channel.send({ embed });
-}
--- commands/lootrules.js
+++ commands/lootrules.js
@@ -1,6 +1,7 @@
 exports.run = function(bot, message, args, discord) {
 		const embed = new discord.RichEmbed().setColor('#6DC066')
 		.setTitle('Regeln für loot, Rollenfarben müssen unterscheidbar von bereits vorhanden Rollen sein')
+		.addField('Custom Role ist immer nur für einen user','Wenn Custo Role gedropped wird, kann Farbe und Name eingestellt werden')
 		.addField('Renaming darf nicht extrem anstossend oder rechtsradikal sein','Falls es unklarheiten gibt, hat ACE immer das letzte wort')
 		message.channel.send({ embed });
 }
